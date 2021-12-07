005fcca89d75dbf5fc598ee9b068a9cb8ab69d7c
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-07 22:41:48 +0500

    json parser updated

--- parse_json.py
+++ parse_json.py
@@ -1,8 +1,9 @@
 import os, json, re
 import nltk
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+from collections import OrderedDict
 
 def parse_json():
     file_path = 'C:/Users/HP/Desktop'
@@ -14,11 +15,12 @@ def parse_json():
         f = open("sample.txt", 'a')
         i = 0
         for raw_data in data_load:
-            raw_data = data_load[i]['content']
-            words = (re.sub('[^a-zA-Z]', ' ', raw_data)).lower().split()
-            processed_data = [snow_stemmer.stem(word) for word in words if not word in set(stopwords.words('english'))]
-            for x in processed_data:
+            text_words = list(OrderedDict.fromkeys((re.sub('[^a-zA-Z]', ' ', data_load[i]['content'])).lower().split()))
+            for x in sorted([snow_stemmer.stem(word) for word in text_words if not word in set(stopwords.words('english'))]):
                 f.write(x + "\n")
             i = i+1
+    f.close()
+
 
 parse_json()
+
