fadc67e73c7d3528c4daefaf3d4ad074955f6862
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-08 01:11:16 +0500

    json parser updated

--- parse_json.py
+++ parse_json.py
@@ -13,13 +13,11 @@ def parse_json():
     for fileName in jsonFiles:
         file = open(('C:/Users/HP/Desktop/{}').format(fileName))
         data_load = json.load(file)
-        f = open("sample.txt", 'a')
         i = 0
         for raw_data in data_load:
             text_words = list(OrderedDict.fromkeys((re.sub('[^a-zA-Z]', ' ', data_load[i]['content'])).lower().split()))
             data += ([snow_stemmer.stem(word) for word in text_words if not word in set(stopwords.words('english'))])
             i = i+1
-    f.close()
     f_lex = open("sample.txt", "w")
     for x in sorted(list(OrderedDict.fromkeys((data)))):
         f_lex.write(x + "\n")
