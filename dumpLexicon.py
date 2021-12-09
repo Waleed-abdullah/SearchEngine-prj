56c08b51857ada9394c85d662ce866e608a961bc
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-09 21:50:49 +0500

    dump lexicon updated

--- dumpLexicon.py
+++ dumpLexicon.py
@@ -1,15 +1,19 @@
-import json
+import json, os
 
 
 # this takes the new words in form of dict as input and creates a new lexicon file with the additions
 def dumpLexicon(input_dict):
-    prevLexicon = open('sample.txt', "r")
-    data = json.load(prevLexicon)
-    for x in input_dict:
-        if x not in data:
+    if os.path.isfile('sample.txt'):
+        prev_lexicon = open('sample.txt', "r")
+        data = json.load(prev_lexicon)
+        for x in input_dict:
+            if x not in data:
+                data[x] = 0
+        prev_lexicon.close()
+    else:
+        data = {}
+        for x in input_dict:
             data[x] = 0
-    prevLexicon.close()
-    newLexicon = open('sample.txt', "w")
-    newLexicon.write(json.dumps(data))
-    newLexicon.close()
-    
+    new_lexicon = open('sample.txt', "w")
+    new_lexicon.write(json.dumps(data))
+    new_lexicon.close()
