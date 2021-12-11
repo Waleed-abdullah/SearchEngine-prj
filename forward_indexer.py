8e2bc4acaf7f1488d644be29c5d5512ec150ebe1
Waleed-Abdullah <w26waleed@gmail.com>
2021-12-11 12:02:58 +0500

    added documnet indexer

--- forward_indexer.py
+++ forward_indexer.py
@@ -6,6 +6,7 @@ from nltk.stem.snowball import SnowballStemmer
 from datetime import datetime
 import zlib
 from dumpLexicon import dumpLexicon
+from Inverted_indexer import generate_inverted_index
 
 
 # This parses json files and creates forward index
@@ -18,73 +19,78 @@ def generate_forward_index(path_to_data):
 
     file_names = [posJson for posJson in os.listdir(path_to_data) if posJson.endswith('.json')]
 
-    temp_forward_index = open("temp_forward_index.txt", 'w')
+    forward_index = open("forward_index.txt", 'w')
     lexicon = {}
+    documentIndices = {}
 
-    for fileName in file_names:
-        file = open('{}/{}'.format(path_to_data, fileName))
+    # load the document indices, if the file exists open in read mode and load the data then open in write mode
+    if os.path.isfile('./document_index.txt'):
+       with open('./document_index.txt') as doc_idx:
+            documentIndices = json.load(doc_idx)
+    
+    
+    document_Index = open('./document_index.txt', 'w')
+
+    # generate the forward index
+    for i in range(1):
+        file = open('./Dataset/nela-gt-2020/newsdata/abcnews.json')
+        #file = open('{}/{}'.format(path_to_data, fileName))
         loaded_data = json.load(file)
 
         for article in loaded_data:
             doc_id = bytes(article['id'], 'utf-8')
-            content = (re.sub('[^a-zA-Z]', ' ', article['content'])).lower().split()
-            stemmed_words = ([snow_stemmer.stem(word) for word in content if not word in stop_words])
+            hashedID = str(zlib.crc32(doc_id))
 
-            position = 1  # position of word in the document
+           # If the article is alredy indexed then continue
+            if hashedID in documentIndices:
+                continue
+            else:
+                documentIndices[hashedID] = article['url']    
+
+            content = (re.sub('[^a-zA-Z]', ' ', article['content'])).lower().split()
+            title = (re.sub('[^a-zA-Z]', ' ', article['title'])).lower().split()
+            stemmed_words = ([snow_stemmer.stem(word) for word in content if not word in stop_words])
+            stemmed_title = ([snow_stemmer.stem(word) for word in title if not word in stop_words])
 
             forward_dict = {}
             hit_list = {}
 
+            # hit list structure is such that word: [0/1, occurances, position]
+            # store the title words in the hit list
+            for word in stemmed_title:
+                if word not in hit_list:
+                    hit_list[word] = [1, 1, 0]
+                else:
+                    hit_list[word][1] += 1
+                if word not in lexicon:
+                    lexicon[word] = 0   
+
+            position = 1  # position of word in the document
+
             for word in stemmed_words:
                 if word not in hit_list:
-                    hit_list[word] = [1, position]
+                    hit_list[word] = [0, 1, position]
                 else:
-                    hit_list[word][0] += 1
+                    hit_list[word][1] += 1
                     hit_list[word].append(position)
                 position += 1
                 if word not in lexicon:
                     lexicon[word] = 0
-            forward_dict['docID'] = zlib.crc32(doc_id)
-            forward_dict['hitList'] = hit_list
-            temp_forward_index.write(json.dumps(forward_dict))
-            temp_forward_index.write('\n')
+            forward_dict[hashedID] = hit_list
+            forward_index.write(json.dumps(forward_dict))
+            forward_index.write('\n')
 
         dumpLexicon(lexicon)
 
-    temp_forward_index.close()
-
-    merge_forward_Index("temp_forward_index.txt")
+    forward_index.close()
+    document_Index.write(json.dumps(documentIndices))
+    document_Index.close()
+    generate_inverted_index()
 
     end = datetime.now()
     print("The time of execution of to create forward index and lexicon is:", str(end - start))
 
 
-
-def merge_forward_Index(fileToBeMerged):
-    # if forward index isnt already created just dump the articles
-    if not os.path.isfile('forward_index.txt'):
-        doc_ids = set()
-        with open('forward_index.txt', 'w') as fwd_idx:
-            with open(fileToBeMerged) as m:
-                for object in m:
-                    document = json.loads(object)
-                    if document['docID'] not in doc_ids:
-                        doc_ids.add(document['docID']) # in case there was a duplicate of a file/article in the dataset
-                        fwd_idx.write(json.dumps(document))
-                        fwd_idx.write('\n')
-    else:
-        doc_ids = set()
-        with open('forward_index.txt', 'r+') as fwd_idx:
-            for object in fwd_idx:
-                document = json.loads(object)
-                doc_ids.add(document['docID'])
-            with open(fileToBeMerged) as m:
-                for object in m:
-                    document = json.loads(object)
-                    if document['docID'] not in doc_ids:
-                        doc_ids.add(document['docID']) # in case there was a duplicate of a file/article in the dataset
-                        fwd_idx.write(json.dumps(document))
-                        fwd_idx.write('\n')
 
 
     
