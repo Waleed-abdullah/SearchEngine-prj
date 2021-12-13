706097bc524d480153f5fe739a9b68a98a547a41
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-13 22:03:01 +0500

    Corrected error in content word positions

--- forward_indexer.py
+++ forward_indexer.py
@@ -6,10 +6,9 @@ from nltk.stem.snowball import SnowballStemmer
 from datetime import datetime
 import zlib
 from dumpLexicon import dumpLexicon
-from Inverted_indexer import generate_inverted_index
 
 
-# This parses json files and creates forward index
+# This parses json files and creates forward index and inverted index
 def generate_forward_index(path_to_data):
 
     start = datetime.now()
@@ -19,33 +18,34 @@ def generate_forward_index(path_to_data):
 
     file_names = [posJson for posJson in os.listdir(path_to_data) if posJson.endswith('.json')]
 
-    forward_index = open("forward_index.txt", 'w')
     lexicon = {}
-    documentIndices = {}
+    document_indices = {}
+    barrel_count = 1
+    doc_count = 0
+    forward_index = open("forward_index_barrel_" + str(barrel_count) + ".txt", 'w')
 
     # load the document indices, if the file exists open in read mode and load the data then open in write mode
     if os.path.isfile('./document_index.txt'):
        with open('./document_index.txt') as doc_idx:
-            documentIndices = json.load(doc_idx)
-    
-    
-    document_Index = open('./document_index.txt', 'w')
+            document_indices = json.load(doc_idx)
 
-    # generate the forward index
-    for i in range(1):
-        file = open('./Dataset/nela-gt-2020/newsdata/abcnews.json')
-        #file = open('{}/{}'.format(path_to_data, fileName))
+    document_index = open('./document_index.txt', 'w')
+
+    for file_name in file_names:
+
+        file = open('{}/{}'.format(path_to_data, file_name))
         loaded_data = json.load(file)
 
         for article in loaded_data:
             doc_id = bytes(article['id'], 'utf-8')
             hashedID = str(zlib.crc32(doc_id))
 
-           # If the article is alredy indexed then continue
-            if hashedID in documentIndices:
+            # If the article is alredy indexed then continue
+            if hashedID in document_indices:
                 continue
             else:
-                documentIndices[hashedID] = article['url']    
+                document_indices[hashedID] = article['url']
+                doc_count += 1
 
             content = (re.sub('[^a-zA-Z]', ' ', article['content'])).lower().split()
             title = (re.sub('[^a-zA-Z]', ' ', article['title'])).lower().split()
@@ -54,16 +54,18 @@ def generate_forward_index(path_to_data):
 
             forward_dict = {}
             hit_list = {}
+            position = 1
 
             # hit list structure is such that word: [0/1, occurances, position]
             # store the title words in the hit list
             for word in stemmed_title:
                 if word not in hit_list:
-                    hit_list[word] = [1, 1, 0]
+                    hit_list[word] = [1, 1, position]
                 else:
                     hit_list[word][1] += 1
                 if word not in lexicon:
-                    lexicon[word] = 0   
+                    lexicon[word] = 0
+                position += 1
 
             position = 1  # position of word in the document
 
@@ -82,15 +84,13 @@ def generate_forward_index(path_to_data):
 
         dumpLexicon(lexicon)
 
-    forward_index.close()
-    document_Index.write(json.dumps(documentIndices))
-    document_Index.close()
-    generate_inverted_index()
+        forward_index.close()
+        document_index.write(json.dumps(document_indices))
+        document_index.close()
 
-    end = datetime.now()
-    print("The time of execution of to create forward index and lexicon is:", str(end - start))
-
+        end = datetime.now()
+        print("The time of execution of to create forward index and lexicon is:", str(end - start))
 
 
 
-    
+
