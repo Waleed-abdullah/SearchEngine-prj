3762becc0ae0f379f420798d14d0c7578fe43d00
Waleed-Abdullah <w26waleed@gmail.com>
2021-12-13 22:42:30 +0500

    fixed an error in the loop

--- Inverted_indexer.py
+++ Inverted_indexer.py
@@ -6,29 +6,32 @@ def generate_inverted_index():
     lexicon = json.load(prev_lex_file)
     prev_lex_file.close()
     # putting an arbitrary value at list[0] so that we can use 1 ... n
-    invertex_idx = []
+    inverted_idx = []
 
     with open('forward_index.txt', 'r') as fwd_idx:
         for object in fwd_idx:
-            document = json.load(object)
-            docID = document.keys()[0]
-            hitList = document[docID]
-            for word, hits in hitList:
-                if lexicon[word]:
-                    invertex_idx[lexicon[word] - 1][word].append([docID, hits])
-                else:
-                    invertex_idx.append({word: []})
-                    lexicon[word] = len(invertex_idx) - 1
+            document = json.loads(object)
+            for docID, hitList in document.items():
+                for word, hits in hitList.items():
+                    if lexicon[word]:
+                        inverted_idx[lexicon[word] - 1][word].append([docID, hits])
+                    else:
+                        inverted_idx.append({word: []})
+                        lexicon[word] = len(inverted_idx)
 
     new_lexicon = open('lexicon.txt', 'w')
     new_lexicon.write(json.dumps(lexicon))
     new_lexicon.close()    
 
     if not os.path.isfile('./inverted_idx.txt'):
-            inverted_idx = open('inverted_idx.txt', 'w')
+        inverted_idx_file = open('inverted_idx.txt', 'w')
     else:
-        inverted_idx = open('inverted_idx.txt', 'a')
+        inverted_idx_file = open('inverted_idx.txt', 'a')
 
+    for wordDict in inverted_idx:
+        inverted_idx_file.write(json.dumps(wordDict))
+        inverted_idx_file.write('\n')
 
+generate_inverted_index()
 
 
