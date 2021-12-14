48152fbd9054893fa0f08a7f27b05f1be9357380
Waleed-Abdullah <w26waleed@gmail.com>
2021-12-14 21:29:09 +0500

    modified searching function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from dumpLexicon import dumpLexicon
-from forward_indexer import generate_forward_index
+from forward_indexer1 import generate_forward_index
 from dumpLexicon import dumpLexicon
 from Inverted_indexer import generate_inverted_index
-
-generate_forward_index('./Dataset/nela-gt-2020/newsdata/')
-generate_inverted_index()
+from searcher import search_word
+# generate_forward_index('./Dataset/nela-gt-2020/newsdata')
+# generate_inverted_index()
+search_word('left')
+search_word('disciplinarian')
+search_word('abuzayd')
+search_word('thapa')
+search_word('magar')
+search_word('sheirff')
--- searcher.py
+++ searcher.py
@@ -6,20 +6,24 @@ from searchLexicon import search_lexicon
 def search_word(word):
     start = datetime.now()
     offset = search_lexicon(word)
-
-    inverted_index = open('inverted_idx.txt', 'r')
-
-    i = 0
-    while i != offset-1:
-        inverted_index.readline()
-        i += 1
-
-    doc_ids = inverted_index.readline()
-    print(doc_ids)
+    if offset == None: 
+        return None
+    wordID = None
+    print('offset', offset)
+    with open('inverted_idx.txt', 'r') as inverted_index:
+        i = 1
+        for line in inverted_index:
+            if i == offset:
+                wordID = json.loads(line)
+                break;
+            i +=1
+            
+    
+    print(wordID.keys())
 
     end = datetime.now()
     print("The time of execution to search a word is:", str(end - start))
 
 
-search_word("disciplinarian")
+
 
