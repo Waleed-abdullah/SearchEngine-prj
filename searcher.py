5a39da763298af95f5d7900ced6800c81bc68747
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-16 15:25:42 +0500

    single word searcher added

---
+++ searcher.py
@@ -0,0 +1,32 @@
+import json, os
+from datetime import datetime
+from searchLexicon import search_lexicon
+
+
+def search_word(word):
+    start = datetime.now()
+
+    word_id = search_lexicon(word)
+
+    if word_id is None:
+        return None
+
+    barrel_num = int(word_id/10000) + 1
+    inverted_index = open("inverted_barrel_" + str(barrel_num) + ".txt", 'r')
+
+    doc_list = []
+    line = json.loads(inverted_index.readline())
+    while line[0][1] != word_id:
+        line = json.loads(inverted_index.readline())
+    while line[0][1] == word_id:
+        doc_list.append([line[0][0], line[1]])
+        line = json.loads(inverted_index.readline())
+
+    print(doc_list)
+
+    end = datetime.now()
+    print("The time of execution to search a word is:", str(end - start))
+
+
+search_word("equiti")
+
