f6a20468be51f19429fa599086a2f3e2fb0a2e73
Usama Qureshi <usama7.qureshi@gmail.com>
2021-12-17 00:51:22 +0500

    added sorter to inverted index file

--- inverted_indexer.py
+++ inverted_indexer.py
@@ -1,23 +1,30 @@
 import os, json
 from datetime import datetime
-from sorter import sort
+
+
+def sort(input_list):
+    sorted_list = [[] for i in range(533)]
+
+    for value in input_list:
+        sorted_list[value[0][1] % 533].append(value)
+
+    return sorted_list
 
 
 def inverted_index_generator():
 
     start = datetime.now()
-    barrels = [forward_barrel for forward_barrel in os.listdir("./forwardBarrels") if forward_barrel.startswith('forward_barrel_')]
-    
+    barrels = [forward_barrel for forward_barrel in os.listdir("./") if forward_barrel.startswith('forward_barrel_')]
 
     for barrel in barrels:
 
-        forward_file = open('./forwardBarrels/{}'.format(barrel))
+        forward_file = open('./{}'.format(barrel))
         inverted_list = []
         # get the barrel number corresponding to the forward index file because they are not sorted
         barrel_num = barrel[15] + barrel[16] if barrel[16].isnumeric() else barrel[15] 
         
-        if os.path.isfile("./InvertedBarrels/inverted_barrel_" + barrel_num + ".txt"):
-            with open("./InvertedBarrels/inverted_barrel_" + barrel_num + ".txt", 'r') as inverted_index:
+        if os.path.isfile("./inverted_barrel_" + barrel_num + ".txt"):
+            with open("./inverted_barrel_" + barrel_num + ".txt", 'r') as inverted_index:
                 for line in inverted_index:
                     inverted_list.append(json.loads(line))
 
@@ -26,7 +33,7 @@ def inverted_index_generator():
         forward_file.close()
 
         # sort the invertedList and write to the inverted barrel
-        inverted_file = open("./InvertedBarrels/inverted_barrel_" + barrel_num + ".txt", 'w')
+        inverted_file = open("./inverted_barrel_" + barrel_num + ".txt", 'w')
         sorted_list = sort(inverted_list)
         for i in range(len(sorted_list)):
             for j in range(len(sorted_list[i])):
