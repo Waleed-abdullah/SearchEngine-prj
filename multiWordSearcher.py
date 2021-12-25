87d6f03c8499e045227f51e2cd5fa0c8c7ad19eb
Waleed-Abdullah <w26waleed@gmail.com>
2021-12-25 17:54:25 +0500

    added rank calculation of documents in multiWord Search based on hits and  also proximity

--- multiWordSearcher.py
+++ multiWordSearcher.py
@@ -20,11 +20,8 @@ def search_words(wordsList):
         word_id = search_lexicon(word)
         if word_id is not None:
             word_ids.append(word_id)
-
-    print(word_ids)
    
-    doc_list = {} #using dict to remove duplicate documents
-
+    documents = {} 
     for word_id in word_ids:
         barrel_num = int(word_id[0] / 533) + 1
         inverted_index = open("./InvertedBarrels/inverted_barrel_" + str(barrel_num) + ".txt", 'r')
@@ -34,15 +31,43 @@ def search_words(wordsList):
         line = json.loads(inverted_index.readline())
         while line[0][1] == word_id[0] and result_count < 31:
             docID = str(line[0][0])
-            doc_list[docID] = doc_list.get(docID, 0) + 1 # storing the frequency of the documents in the dict
+            titleHitList = line[1][0]
+            titleHits = titleHitList[1]
+            contentHitList = line[1][1]
+            contentHits = contentHitList[1]
+            if docID in documents:
+                # add the new hits to the score
+                    documents[docID][0] = documents[docID][0] + titleHits +  contentHits
+                # calculate proximity and add weight
+                    if contentHits > 0:
+                        if documents[docID][1] is not None:
+                            proximity = abs(documents[docID][1][2] - contentHitList[2])
+                            if proximity <= 1:
+                                documents[docID][0] += 10
+                            elif proximity <= 10:
+                                documents[docID][0] += 8
+                            elif proximity <= 100:
+                                documents[docID][0] += 4
+                            else:
+                                documents[docID][0] += 2
+                        #add the hitlist of the current word for next words proximity calculation
+                        documents[docID][1] = contentHitList
+            else:                       
+                    if contentHits > 0:
+                        documents[docID] = [titleHits + contentHits, contentHitList]  # add hits in both title and content and store the hit list for proximity check
+                    else:
+                        documents[docID] = [titleHits + contentHits, None] 
             line = json.loads(inverted_index.readline())
             result_count += 1
 
         inverted_index.close()
 
+    # convert the documents dictionary into a list and sort in descending order based on the score | higher the score the higher the rank of the document
 
-    print(len(doc_list))
-    print(doc_list)
+    rankedDocuments = sorted(list(documents.items()), key = lambda x: x[1][0], reverse = True)
+
+    print(len(rankedDocuments))
+    print(rankedDocuments)
     end = datetime.now()
     print("The time of execution to search a word is:", str(end - start))
 
